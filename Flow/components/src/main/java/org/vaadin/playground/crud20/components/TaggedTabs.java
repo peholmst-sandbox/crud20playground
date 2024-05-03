8628835bba19df0cda7d8e133145fa7535235b19
Petter Holmström <petter@vaadin.com>
2024-05-03 16:43:30 +0300

    Add TaggedTabs

---
+++ Flow/components/src/main/java/org/vaadin/playground/crud20/components/TaggedTabs.java
@@ -0,0 +1,31 @@
+package org.vaadin.playground.crud20.components;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaggedTabs<TAG> extends Composite<Tabs> implements HasSize {
+
+    private final Map<TAG, Tab> idToTab = new HashMap<>();
+
+    public final void add(@Nonnull TAG tag, @Nonnull Tab tab) {
+        if (idToTab.putIfAbsent(tag, tab) != null) {
+            throw new IllegalArgumentException("Id has already been used");
+        }
+        getContent().add(tab);
+    }
+
+    public final void select(@Nullable TAG tag) {
+        if (tag == null) {
+            getContent().setSelectedTab(null);
+        } else {
+            getContent().setSelectedTab(idToTab.get(tag));
+        }
+    }
+}
